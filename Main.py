1b61bcc4383f9f7d16f99c1cb299121b7bfa7408
Luana Carbonera <luana.lcarbonera@gmail.com>
2021-10-10 16:15:09 -0300

    keeping the display on the screen

--- Main.py
+++ Main.py
@@ -3,5 +3,12 @@ import pygame
 pygame.init()
 pygame.display.set_mode((400, 400))
 pygame.display.update()
+pygame.display.set_caption("Snake Game")
+
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        print(event)
+
 pygame.quit()
 
