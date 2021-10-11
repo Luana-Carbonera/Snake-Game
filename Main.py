c54728a199b4c0db30276889df2d1c5862983568
Luana Carbonera <luana.lcarbonera@gmail.com>
2021-10-11 19:26:08 -0300

    Create Snake

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ pygame.init()
 screen = pygame.display.set_mode((400, 400))
 game_over = False
 color = (255, 255, 255)
+color_snake = (0, 128, 0)
 screen.fill(color)
 pygame.display.update()
 pygame.display.set_caption("Snake Game")
@@ -13,7 +14,8 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-
+        pygame.draw.rect(screen, color_snake, pygame.Rect(200, 200, 10, 10))
+    pygame.display.update()
 
 pygame.quit()
 
