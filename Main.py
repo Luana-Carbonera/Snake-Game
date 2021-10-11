0ad7a1fe0087c74503ee25083f76f1204956f7c7
Luana Carbonera <luana.lcarbonera@gmail.com>
2021-10-11 19:14:14 -0300

    Adding background color

--- Main.py
+++ Main.py
@@ -1,15 +1,19 @@
 import pygame
 
 pygame.init()
-pygame.display.set_mode((400, 400))
+
+screen = pygame.display.set_mode((400, 400))
+game_over = False
+color = (255, 255, 255)
+screen.fill(color)
 pygame.display.update()
 pygame.display.set_caption("Snake Game")
 
-game_over = False
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+
 
 pygame.quit()
 
