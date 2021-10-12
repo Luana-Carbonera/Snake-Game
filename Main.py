e5bd4b9e9cca4116c03526e538ceb55305af497c
Luana Carbonera <luana.lcarbonera@gmail.com>
2021-10-11 21:04:58 -0300

    Moving Snake

--- Main.py
+++ Main.py
@@ -6,6 +6,11 @@ screen = pygame.display.set_mode((400, 400))
 game_over = False
 color = (255, 255, 255)
 color_snake = (0, 128, 0)
+x1 = 200
+y1 = 200
+x = 0
+y = 0
+clock = pygame.time.Clock()
 screen.fill(color)
 pygame.display.update()
 pygame.display.set_caption("Snake Game")
@@ -14,8 +19,27 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-        pygame.draw.rect(screen, color_snake, pygame.Rect(200, 200, 10, 10))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                y = -10
+                x = 0
+            elif event.key == pygame.K_DOWN:
+                y = 10
+                x = 0
+            elif event.key == pygame.K_LEFT:
+                x = -10
+                y = 0
+            elif event.key == pygame.K_RIGHT:
+                x = 10
+                y = 0
+
+
+    x1 += x
+    y1 += y
+    screen.fill(color)
+
+    pygame.draw.rect(screen, color_snake, pygame.Rect(x1, y1, 10, 10))
     pygame.display.update()
+    clock.tick(15)
 
 pygame.quit()
-
